4c559c9b17b8d5c9556020a88e1c9974a4f9bbeb
SquadxMations <106130950+SquadxMations@users.noreply.github.com>
2023-02-11 12:31:09 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 10, 2023',
+  birthdate: 'Feb 12, 2023',
   name: 'Kevin'
 };
 
